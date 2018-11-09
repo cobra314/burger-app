e534a4811d08551e55a51aadaa86f6dcae1fcab1
cobra314 <toledo.ing@gmail.com>
2018-11-08 21:19:17 -0600

    change Aux folder name

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux/Aux2';
+import Aux from '../../../hoc/Aux2/Aux2';
 
 const SideDrawer = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import Aux from '../Aux/Aux2';
+import Aux from '../Aux2/Aux2';
 import classes from './Layout.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
